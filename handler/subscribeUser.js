cb60737fc5c80f289fd943d7264ef606fdf6f099
Fremen1990 <fremen1990@gmail.com>
2022-05-27 20:19:56 +0200

    subscribeUser function completed, deployed and tested.

---
+++ handler/subscribeUser.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk');
+const USERS_TABLE = process.env.USERS_TABLE
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+const uuid = require('uuid')
+
+module.exports.subscribeUser = (event, context, callback) => {
+	const data = JSON.parse(event.body);
+	console.log("EVENT:::", data);
+
+	const timestamp = new Date().getTime();
+
+	if (typeof data.email !== "string") {
+		console.error("Validation error");
+		return;
+	}
+	const params = {
+		TableName: USERS_TABLE,
+		Item: {
+			userId: uuid.v4(),
+			email: data.email,
+			subscriber: true,
+			createdAt: timestamp,
+			updatedAt: timestamp
+		}
+	}
+//	write the user to the database DynamoDB
+
+	dynamoDb.put(params, (error, data) => {
+		if (error) {
+			console.error(error)
+			callback(new Error(error))
+			return;
+		}
+		// create response
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify(data.Item)
+		}
+		callback(null, response)
+
+	})
+
+}
